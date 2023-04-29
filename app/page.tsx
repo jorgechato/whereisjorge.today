58b171a06b05423ba278c850d867c6d965538728
jorgechato <hack@jorgechato.com>
2023-04-29 13:37:57 +0900

    UPDATE basic styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <>
             <section className="mb-12">
                 <div
-                    className="bg-accent border-radius-5 alert alert-dismissible fade show rounded-lg py-10 px-6 text-white md:flex justify-between items-center text-center md:text-left">
+                    className="bg-accent border-radius-5 alert alert-dismissible fade show rounded-lg py-6 px-6 text-white md:flex justify-between items-center text-center md:text-left transition duration-300 hover:scale-105 cursor-pointer">
                     <div className="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
                         <strong className="mr-1">Page under maintenance</strong> We will come back soon, better and stronger.
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import colors from 'tailwindcss/colors';
 const tailwindConfig = require('@jorgechato/manyo/tailwind.config.js');
 
 
-tailwindConfig.theme.extend.colors.accent = colors.red[600];
+tailwindConfig.theme.extend.colors.accent = colors.blue[600];
 tailwindConfig.content = [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
