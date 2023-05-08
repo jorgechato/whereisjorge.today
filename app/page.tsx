df5f81f6fbb5a1dca787b25dce3b1f512903ed3d
jorgechato <hack@jorgechato.com>
2023-05-08 10:53:08 +0900

    DELETE WIP alert

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,6 @@ export default function Home() {
     return (
         <>
             <section className="mb-12">
-                <div
-                    className="bg-accent border-radius-5 alert alert-dismissible fade show rounded-lg py-6 px-6 text-white md:flex justify-between items-center text-center md:text-left transition duration-300 hover:scale-105 cursor-pointer">
-                    <div className="mb-4 md:mb-0 flex items-center flex-wrap justify-center md:justify-start">
-                        <strong className="mr-1">Page under maintenance</strong> We will come back soon, better and stronger.
-                    </div>
-                </div>
             </section>
         </>
     )
