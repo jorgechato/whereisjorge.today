3d22d3a4e6374b641bb44eaf6b8a2e746177f80c
jorgechato <hack@jorgechato.com>
2023-05-08 11:02:13 +0900

    FIX healthcheck & UPDATE version

--- app/api/health-check/route.ts
+++ app/api/health-check/route.ts
@@ -3,13 +3,16 @@ import { NextResponse } from 'next/server';
 import { lib } from '@jorgechato/manyo';
 import packageInfo from '@/package.json';
 
+const config: { [key: string]: any } = require('@/my.config.js');
+
 
 export async function GET() {
     const status: lib.StatusPage = {
-        name: 'WHERE IS</br>JORGE TODAY?',
+        name: config.TITLE,
         type: lib.StatusPageCode.OK,
         url: 'https://whereisjorge.today',
         version: packageInfo.version,
+        description: packageInfo.description,
     };
 
     return NextResponse.json(status, {
