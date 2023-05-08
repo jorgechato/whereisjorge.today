c2438f57b5cf3a3caf6395bf167a2b2fd947cdaa
jorgechato <hack@jorgechato.com>
2023-05-08 10:52:42 +0900

    ADD location banner in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,19 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 import { Header, Footer } from '@jorgechato/manyo';
+import { Banner } from '@/components/location/Banner';
 
 
 const config: { [key: string]: any } = require('@/my.config.js');
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    const {TITLE, SITE_MAP, SOCIALS} = config;
+    const { TITLE, SITE_MAP, SOCIALS } = config;
 
     return (
         <html lang="en" className="overflow-x-hidden dark">
             <body className="bg-bg antialiased font-body leading-7 text-body">
-                <Header title={TITLE} />
+                <Header title={TITLE} className="!mb-0 !sm:mb-0 shadow-xs" />
+                <Banner name="Jorge"/>
                 <main id="content">
                     <div className="container mx-auto px-4">
                         {children}
