e5f331953361d8036b8dbd4fe556ecb72139fdd0
jorgechato <hack@jorgechato.com>
2024-03-20 11:22:27 +0900

    change(location): overwrite location thumbnail

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -4,11 +4,16 @@ import { lib } from '@jorgechato/manyo';
 
 export const dynamic = 'force-dynamic';
 
+const config: { [key: string]: any } = require('@/locations.js');
+
 
 export async function GET() {
-    const location: lib.NomadList|null = await lib.GetLocation();
+    const nomad: lib.NomadList | null = await lib.GetLocation();
+    if (nomad != null) {
+        nomad.location.now.thumbnail = config[nomad?.location.now.city] ?? nomad.location.now.thumbnail;
+    }
 
-    return NextResponse.json(location,{
+    return NextResponse.json(nomad, {
         headers: {
             'Content-Type': 'application/json',
             'Cache-Control': 'stale-while-revalidate=86400, s-maxage=86400, max-age=86400', // 24h cache
---
+++ locations.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "Osaka": "https://images.unsplash.com/photo-1590559899731-a382839e5549?crop=entropy&cs=tinysrgb&fm=jpg&h=640&w=500&fit=cover",
+  "Tokyo": "https://images.unsplash.com/photo-1516292698305-5cba99c9f9c5?crop=entropy&cs=tinysrgb&fm=jpg&h=640&w=500&fit=cover",
+  "Kyoto": "https://images.unsplash.com/photo-1567870966759-48a5731167e7?crop=entropy&cs=tinysrgb&fm=jpg&h=640&w=500&fit=cover",
+};
